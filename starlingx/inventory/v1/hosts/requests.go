46bde3ee42047bbcc38dddd53f9075904507c7f9
Susendra Selvaraj <Susendra.Selvaraj@windriver.com>
2023-01-23 05:44:32 -0500

    Check Commit for AppArmor
    AppArmor enabling using DM
    
    Signed-off-by: Susendra Selvaraj <Susendra.Selvaraj@windriver.com>

--- starlingx/inventory/v1/hosts/requests.go
+++ starlingx/inventory/v1/hosts/requests.go
@@ -32,6 +32,7 @@ type HostOpts struct {
 	Action               *string       `json:"action,omitempty" mapstructure:"action"`
 	MaxCPUMhzConfigured  *string       `json:"max_cpu_mhz_configured,omitempty" mapstructure:"max_cpu_mhz_configured"`
 	AppArmor             *string       `json:"apparmor,omitempty" mapstructure:"apparmor"`
+	AppArmor1             *string       `json:"apparmor,omitempty" mapstructure:"apparmor"`
 }
 
 const (
